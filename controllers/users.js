5ff7d0e24ecbd866c096675cd9ff5fae086479db
Anastasia Kapustina <nikitinaan98@mail.ru>
2023-07-01 01:12:30 +0300

    set secure

--- controllers/users.js
+++ controllers/users.js
@@ -51,8 +51,8 @@ module.exports.createUser = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 1000 * 60 * 60 * 24 * 7,
           httpOnly: true,
-          sameSite: 'lax',
-          domain: 'http://movies-kapustina.nomoreparties.sbs',
+          sameSite: 'none',
+          secure: true,
         })
         .status(201)
         .send({ data: email })
@@ -75,8 +75,8 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 1000 * 60 * 60 * 24 * 7,
           httpOnly: true,
-          sameSite: 'lax',
-          domain: 'http://movies-kapustina.nomoreparties.sbs',
+          sameSite: 'none',
+          secure: true,
         })
         .send({ data: email })
         .end();
@@ -89,8 +89,8 @@ module.exports.logout = (req, res) => {
     .cookie('jwt', '', {
       maxAge: 0,
       httpOnly: true,
-      sameSite: 'lax',
-      domain: 'http://movies-kapustina.nomoreparties.sbs',
+      sameSite: 'none',
+      secure: true,
     })
     .send({ data: SIGNOUT_MESSAGE });
 };
